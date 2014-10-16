e0a878314ef8f19b2dfad0921e6a5c9814757d99
Jordan Blacker <jordan.blacker@gatech.edu>
2014-10-15 22:46:42 -0400

    Rewrote EarthSim.Demo to build config, display errors, and start GUI

--- src/EarthSim/Demo.java
+++ src/EarthSim/Demo.java
@@ -1,12 +1,28 @@
 package EarthSim;
 
+import EarthPresentation.MasterGui;
+import core.Config;
+
 public class Demo {
 	/* 
 	 * Starts Heated Earth application
 	 */
     public static void main(String[] args) {
-    	// Run Simulator
-    	
+    	try{
+    		Config.buildGlobalConfig(args);
+    		MasterGui.start();
+    	}
+    	catch(IllegalArgumentException ex) {
+    		System.out.print("Could not start application: ");
+    		System.out.println(ex.getMessage());
+    		System.out.println();
+    		System.out.println(getSyntaxMessage());
+    	}
+    }
+    
+    public static String getSyntaxMessage() {
+    	//TODO: Create message on proper usage of arguments for error display;
+    	return null;
     }
    
 }
